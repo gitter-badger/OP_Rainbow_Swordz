4fda5eb999092882dcff4b8e5a7e34137e7fa71c
wooden-utensil-GitDesktop <Mark3001@MARK>
2019-04-09 10:03:53 +1000

    remotetnt block

---
+++ Items/Placeable/RemoteTNT.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using static Terraria.Localization.GameCulture;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace OP_Rainbow_Swordz.Items.Placeable
+{
+	public class ExampleBlock : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Remote TNT");
+			Tooltip.SetDefault("Use with TNT detonator.");
+		}
+
+		public override void SetDefaults() {
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 5;
+			item.useTime = 999;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = mod.TileType("TNT");
+		}
+
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
